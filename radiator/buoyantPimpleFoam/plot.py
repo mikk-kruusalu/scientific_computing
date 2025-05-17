ecf52ca1d36a14d2e170432c07b5a487d32f7134
Mikk Kruusalu <kruusalu.mikk@gmail.com>
2025-05-17 10:23:04 +0300

    Paralleelse skaleerimise tulemus

---
+++ radiator/buoyantPimpleFoam/plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+df = pd.read_csv("parallel-run.csv")
+print(df)
+
+plt.plot(df["cores"], df["elapsed_time"]/60/60, "o-")
+plt.xlabel("cores")
+plt.ylabel("hours")
+plt.savefig("speedup.png")
