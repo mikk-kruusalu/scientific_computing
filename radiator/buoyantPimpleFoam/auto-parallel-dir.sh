e70fea770c6ef21e4dfcf8a4729547f8df507c5f
HardiVv <hardiveski@gmail.com>
2025-05-18 11:15:25 +0300

    automated working

--- radiator/buoyantPimpleFoam/auto-parallel-dir.sh
+++ radiator/buoyantPimpleFoam/auto-parallel-dir.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# chmod +x ./parallel-dir.sh # make exicutables
-# chmod +x ./withstl-parallel/parallel-test.sh
+# Run mesh.slurm first in withstl-paraller folder
+# cd withstl-parallel
+# sbatch mesh.slurm
+# sleep 1080 # wait ~18 min
+# cd ..
 
-for cores in $(seq 8 4 64); do
+# optimal core number is where the ratio of cores = (x y z)
+# is close to one
+# optimal_cores=(16 24 27 32 36 48 54 60 64 72 80 81 96 108 120 125)
+optimal_cores=(27 32 36 48 54 60 64 72)
+
+for cores in "${optimal_cores[@]}"; do
     echo "Setting up test with $cores cores"
     ./parallel-dir.sh $cores
     cd withstl-parallel-$cores
-
     ./parallel-test.sh $cores
-    
     cd ..
 done
 
